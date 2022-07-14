5498cb4dcf72b640297433497978e86b7e08b77f
Florian <florian@florian.me.uk>
2022-07-14 20:28:10 +0200

    Concurrent fetching of remote data

---
+++ welsh_csc/click_ext.py
@@ -0,0 +1,123 @@
+import click
+import urllib.request
+import urllib.error
+from typing import Container, Final
+
+
+HTTP_STATUS_CODES: Final[dict[int, str]] = {
+    100: "Continue",
+    101: "Switching protocols",
+    102: "Processing",
+    103: "Early Hints",
+    200: "OK",
+    201: "Created",
+    202: "Accepted",
+    203: "Non-Authoritative Information",
+    204: "No Content",
+    205: "Reset Content",
+    206: "Partial Content",
+    207: "Multi-Status",
+    208: "Already Reported",
+    226: "IM Used",
+    300: "Multiple Choices",
+    301: "Moved Permanently",
+    302: "Found",
+    303: "See Other",
+    304: "Not Modified",
+    305: "Use Proxy",
+    306: "Switch Proxy",
+    307: "Temporary Redirect",
+    308: "Permanent Redirect",
+    400: "Bad Request",
+    401: "Unauthorized",
+    402: "Payment Required",
+    403: "Forbidden",
+    404: "Not Found",
+    405: "Method Not Allowed",
+    406: "Not Acceptable",
+    407: "Proxy Authentication Required",
+    408: "Request Timeout",
+    409: "Conflict",
+    410: "Gone",
+    411: "Length Required",
+    412: "Precondition Failed",
+    413: "Payload Too Large",
+    414: "URI Too Long",
+    415: "Unsupported Media Type",
+    416: "Range Not Satisfiable",
+    417: "Expectation Failed",
+    418: "I'm a Teapot",
+    421: "Misdirected Request",
+    422: "Unprocessable Entity",
+    423: "Locked",
+    424: "Failed Dependency",
+    425: "Too Early",
+    426: "Upgrade Required",
+    428: "Precondition Required",
+    429: "Too Many Requests",
+    431: "Request Header Fields Too Large",
+    451: "Unavailable For Legal Reasons",
+    500: "Internal Server Error",
+    501: "Not Implemented",
+    502: "Bad Gateway",
+    503: "Service Unavailable",
+    504: "Gateway Timeout",
+    505: "HTTP Version Not Supported",
+    506: "Variant Also Negotiates",
+    507: "Insufficient Storage",
+    508: "Loop Detected",
+    510: "Not Extended",
+    511: "Network Authentication Required",
+}
+
+
+class URLParamType(click.ParamType):
+    name = "URL"
+
+    def __init__(
+        self,
+        permitted_schemes: Container[str] | None = None,
+        default_scheme: str = "http",
+        ignore_http_errors: bool = True
+    ) -> None:
+        super().__init__()
+        self.permitted_schemes = permitted_schemes
+        self.default_scheme = default_scheme
+        self.ignore_http_errors = ignore_http_errors
+
+    def convert(self, value: str, param: click.Parameter | None, ctx: click.Context | None) -> str:
+        value = ("://" not in value) * f"{self.default_scheme}://" + value
+
+        scheme, _ = value.split("://", maxsplit=1)
+        if self.permitted_schemes and scheme.lower() not in self.permitted_schemes:
+            self.fail(f"The URL {scheme!r} is not supported for this parameter")
+
+        try:
+            with urllib.request.urlopen(value) as _:
+                pass
+        except urllib.error.HTTPError as e:
+            if not self.ignore_http_errors:
+                self.fail(
+                    f"The URL {value!r} returned an HTTP Error ([Error {e.code}] {e.reason})",
+                    param, ctx
+                )
+        except urllib.error.URLError as e:
+            self.fail(f"{value!r} is not a valid URL or cannot be opened ({e.reason})", param, ctx)
+
+        return value
+
+
+def report_http_error(url: str, status_code: int):
+    click.echo("The URL ", nl=False, err=True)
+    click.secho(url, fg="blue", nl=False, err=True)
+    click.echo(" returned an HTTP Error (", nl=False, err=True)
+    click.secho(f"{status_code} {HTTP_STATUS_CODES[status_code]}", fg="yellow", nl=False, err=True)
+    click.echo(")")
+
+
+def report_url_error(url: str, message: str):
+    click.echo("The URL ", nl=False, err=True)
+    click.secho(url, fg="blue", nl=False, err=True)
+    click.echo(" is not a valid URL or could not be opened (", nl=False, err=True)
+    click.secho(message, fg="yellow", nl=False, err=True)
+    click.echo(")")
---
+++ welsh_csc/get_data.py
@@ -0,0 +1,142 @@
+import click
+import click_spinner
+import requests
+import concurrent.futures
+import re
+from pathlib import Path
+from .click_ext import URLParamType, report_http_error, report_url_error
+
+
+@click.option(
+    "-d", "--dest",
+    type=click.Path(
+        exists=True, file_okay=False, resolve_path=True, writable=True, path_type=Path
+    ),
+    default="./data", show_default=True,
+    help="The directory where the data files should be stored."
+)
+@click.option(
+    "-c", "--channel", type=click.Choice(["1", "2", "1+2"]),  default="1+2", show_default=True,
+    help="Whether to obtain audio-only (1 ch), audio and lx (2 ch), or both (1+2 ch) versions."
+)
+@click.option(
+    "-r", "--remote",
+    type=URLParamType(
+        permitted_schemes=("http", "https"), ignore_http_errors=False
+    ),
+    default="https://data.ling101.com/welsh-csc/data/", show_default=False,
+    help="URL for the remote server from which to fetch data. [default: https://data.ling101.com]"
+)
+@click.argument("component", type=click.Choice(["all", "raw", "chopped"], case_sensitive=False))
+def get_data(component: str, dest: Path, channel: str, remote: str):
+    """Retrieve the Welsh CSC data from remote host.
+
+    Existing data files will not be overwritten. To replace files, delete them first (or rename
+    them by adding *.bak at the end).
+
+    The destination path must already exist and be writeable. By default ./data is assumed
+    (relative to the current working directory). Depending on the provided options, subdirectories
+    with the names raw-1ch, raw-2ch, chopped-1ch, chopped-2ch may be created.
+    """
+    # Compute the assets (folders) to be downloaded...
+    components = ["raw", "chopped"] if component == "all" else [component]
+    channels = ["1", "2"] if channel == "1+2" else [channel]
+    targets = [f"{component}-{channel}ch" for channel in channels for component in components]
+    # Report parameters to user
+    click.secho("Assets to be downloaded: ", bold=True, nl=False)
+    click.echo(", ".join(click.style(target, fg="yellow") for target in targets))
+    click.secho("Destination directory: ", bold=True, nl=False)
+    click.secho(dest, fg="yellow")
+    for target in targets:
+        remote_url = f"{remote}{target}" if remote.endswith("/") else f"{remote}/{target}"
+        destination = dest / target
+        destination.mkdir(exist_ok=True)
+        _get_data(remote_url, destination)
+
+
+def _get_data(remote_url: str, destination: Path):
+    click.secho("Fetching files from remote: ", bold=True, nl=False)
+    click.secho(remote_url, bold=True, fg="blue")
+    index = _build_remote_index(remote_url)
+    if not index:
+        return
+    operands = zip(index, map(lambda s: destination / s.split("/")[-1], index))
+    session = requests.Session()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = {
+            executor.submit(
+                _download_file, remote, local, session=session
+            ): remote for remote, local in operands
+        }
+        errors: dict[str, BaseException] = {}
+        with click.progressbar(
+            length=len(futures),
+            label='Downloading files',
+            show_eta=True,
+            show_pos=True,
+            bar_template=click.style('%(label)s  [%(bar)s]  %(info)s', dim=True),
+            color=True
+        ) as pbar:  # type: ignore
+            for future in concurrent.futures.as_completed(futures):
+                pbar.update(1)  # type: ignore
+                if error := future.exception():
+                    err_url = futures[future]
+                    errors[err_url] = error
+        for err_url, error in errors.items():
+            click.secho("ERROR: ", bold=True, fg="red", nl=False, err=True)
+            if isinstance(error, requests.HTTPError):
+                report_http_error(err_url, error.response.status_code)
+            else:
+                report_url_error(err_url, str(error))
+
+
+def _download_file(
+    remote_url: str,
+    destination: Path,
+    chunk_size: int = 1_048_576,
+    session: requests.Session | None = None
+):
+    with destination.open("wb", chunk_size*5) as fp:
+        getter = session.get if session else requests.get
+        r = getter(remote_url, stream=True)
+        r.raise_for_status()
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            fp.write(chunk)
+
+
+def _build_remote_index(remote_url: str) -> list[str]:
+    index_stack: list[str] = [remote_url]
+    item_stack: list[str] = []
+    err_stack: list[str] = []
+    click.secho("Building index... ", dim=True, nl=False)
+    with requests.Session() as session:
+        with click_spinner.spinner():  # type: ignore
+            while index_stack:
+                target_url = index_stack.pop()
+                target_url += (not target_url.endswith("/")) * "/"
+                r = session.get(target_url, params={"F": 0})
+                if r.status_code == requests.codes.ok:
+                    items = _parse_apache_index(r.text)
+                    for item in items:
+                        item = f"{target_url}{item}"
+                        if item.endswith("/"):
+                            index_stack.append(item)
+                        else:
+                            item_stack.append(item)
+                else:
+                    err_stack.append(target_url)
+    click.echo(f"{len(item_stack)} files to fetch")
+    for err_url in err_stack:
+        click.secho("ERROR: ", bold=True, fg="red", nl=False, err=True)
+        click.echo("Could not obtain index for remote directory at ", nl=False, err=True)
+        click.secho(err_url, fg="blue", err=True)
+    return item_stack
+
+
+def _parse_apache_index(html: str) -> list[str]:
+    matches = re.findall(
+        r"^<li><a href\=\"([\w\-\.\ \%]+/?)\">",
+        html,
+        flags=re.M
+    )
+    return matches or []
